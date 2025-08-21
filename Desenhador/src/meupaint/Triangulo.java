c9c5183f795603fa9344ee02c47a32ec584871cf
EddiePricefield <eduardofsantana17@gmail.com>
2025-08-21 14:08:28 -0300

    Implementei um jeito de definir se a ponta do triângulo vai estar para cima ou para baixo

--- Desenhador/src/meupaint/Triangulo.java
+++ Desenhador/src/meupaint/Triangulo.java
@@ -20,29 +20,33 @@ public class Triangulo extends Forma{ //Ao invés de fazer uma lista para cada c
     @Override
     public void desenhar( Graphics g ){
         
-       //Pegando os pontos de um retângulo com base nos pontos X e Y do mouse
-       int x1d = x1 < x2 ? x1 : x2;
-       int y1d = y1 < y2 ? y1 : y2;
+        //Pegando os pontos de um retângulo com base nos pontos X e Y do mouse
+        int x1d = x1 < x2 ? x1 : x2;
+        int y1d = y1 < y2 ? y1 : y2;
        
-       int x2d = x1 > x2 ? x1 : x2;
-       int y2d = y1 > y2 ? y1 : y2;
+        int x2d = x1 > x2 ? x1 : x2;
+        int y2d = y1 > y2 ? y1 : y2;
        
-       //Construindo um triângulo isósceles com base no retângulo construído pelo mouse
-       
-       xPoints = new int[]{x1d, ( x2d + x1d ) / 2, x2d};
-       yPoints = new int[]{y2d, y1d, y2d} ;      
-       
-       Graphics2D g2 = ( Graphics2D ) g.create();
+        //Construindo um triângulo isósceles com base no retângulo construído pelo mouse
+        if( y1 > y2 ){ //Ponta para CIMA
+            xPoints = new int[]{x1d, ( x2d + x1d ) / 2, x2d};
+            yPoints = new int[]{y2d, y1d, y2d} ;
+        } else{ //Ponta para BAIXO
+            xPoints = new int[]{x1d, (x2d + x1d) / 2, x2d};
+            yPoints = new int[]{y1d, y2d, y1d};
+        }
+             
+        Graphics2D g2 = ( Graphics2D ) g.create();
        
        //da pra usar um IF caso ele seja uma cor de preenchimento nula, pra nao usar o preenchimento só o contorno
        
-       g2.setStroke(new BasicStroke(tamanhoContorno));
-       g2.setColor( preenchimento );
-       g2.fillPolygon(xPoints, yPoints, 3);
-       g2.setColor( contorno );
-       g2.drawPolygon(xPoints, yPoints, 3);
+        g2.setStroke(new BasicStroke(tamanhoContorno));
+        g2.setColor( preenchimento );
+        g2.fillPolygon(xPoints, yPoints, 3);
+        g2.setColor( contorno );
+        g2.drawPolygon(xPoints, yPoints, 3);
        
-       g2.dispose();
+        g2.dispose();
         
     }
     
